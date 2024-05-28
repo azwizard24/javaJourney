6daf749943993742d343c9a6ca27c57a4ed814fb
Abbasi <erfan.abbasi@adesso.de>
2024-05-28 22:14:39 +0200

    First Commit

---
+++ FirstClass.java
@@ -0,0 +1,52 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        /*
+        boolean bool = true; // 1 bit
+        byte b = 1; // 1 byte
+        short s = 1; // 2 bytes
+        int i = 1; // 4 bytes
+        long l = 1; // 8 bytes
+
+        double d = 1; // 8 bytes
+        char c = 'c'; // 2 bytes
+        String str = "Erfan";
+
+        String x = "Water";
+        String y = "Kool-Aid";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = x;
+
+        System.out.println("x: "+ x);
+        System.out.println("y: "+ y);
+
+        System.out.println("Hello World");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello "+ name);
+        System.out.println("Your age is "+age);
+
+
+        double friends = 10;
+
+        friends++;
+
+        System.out.println(friends);
+        */
+
+
+
+    }
+}
